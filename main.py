b64b0f86a3fb053a62d0bb66d771f54378417d55
Susi <kellersu@student.ethz.ch>
2023-07-04 14:23:59 +0200

    reformat as main() function

--- main.py
+++ main.py
@@ -45,27 +45,6 @@ snapshot_freq = 5000
 sample_freq = 2000
 
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-checkpoint_dir = os.path.join(os.getcwd(), "checkpoint","base")
-checkpoint_meta_dir = os.path.join(
-        os.getcwd(), "checkpoint-meta","base", "checkpoint.pth")
-sample_dir = os.path.join(os.getcwd(), "samples_dir", "base")
-
-
-
-
-"""Instantiate dataloaders of training and test datasets"""
-transform = [transforms.Resize(IMG_SIZE),
-                 transforms.CenterCrop(IMG_SIZE), transforms.ToTensor()]
-transform = transforms.Compose(transform)
-training_data = datasets.MNIST(
-            root=".", train=True, download=True, transform=transform)
-test_data = datasets.MNIST(
-            root=".", train=False, download=True, transform=transform)
-
-
-trainloader = DataLoader(training_data, batch_size=BATCH_SIZE, shuffle=True, drop_last=True)
-testloader = DataLoader(test_data, batch_size=BATCH_SIZE, shuffle=True, drop_last=True)
 
 
 """Define the loss function"""
@@ -120,16 +99,17 @@ def save_checkpoint(ckpt_dir, state):
         }
     torch.save(saved_state, ckpt_dir)
     
-def sample():
+def sample(model, test_data, device):
     plt.clf()
     testloader = DataLoader(test_data, batch_size=BATCH_SIZE, shuffle=True, drop_last=True)
     sample_img, label = next(iter(testloader))
+    sample_img = sample_img.to(device)
     pred = model(sample_img)
     for i in range(0,6):
         
         plt.subplot(2,3,i+1)
         plt.tight_layout()
-        plt.imshow(sample_img[i][0], cmap='gray', interpolation='none')
+        plt.imshow(sample_img.cpu()[i][0], cmap='gray', interpolation='none')
         plt.title(torch.argmax(pred[i]))
         plt.xticks([])
         plt.yticks([])
@@ -168,88 +148,110 @@ def get_step_fn(train,optimize_fn=optimization_manager(),
 
     return step_fn
 
-
-model = resnet50()
-model.conv1 = torch.nn.Conv2d(1,64, kernel_size=(7,7), stride = (2,2), padding = (3,3), bias = False)
-
-model = nn.Sequential(model, nn.Linear(1000,10), nn.LogSoftmax(1))
-
-
-model.to(device)
-
-optimizer = Adam(model.parameters(), lr=0.001)
-
-
-
-"""Initialise model state"""
-
-state = dict(optimizer=optimizer, model=model, step=0)
-state = restore_checkpoint(checkpoint_meta_dir, state, device)
-optimize_fn = optimization_manager()
-train_step_fn = get_step_fn(train=True,optimize_fn=optimize_fn)
-eval_step_fn = get_step_fn(train=False,optimize_fn=optimize_fn)
-
-
-initial_step = int(state['step'])
-train_iter = iter(trainloader)
-eval_iter = iter(testloader)
-
-train = False
-
-if (train == True):
-
-    for step in range(initial_step, num_train_steps + 1):
-        # Train step
-        try:
-            batch, labels = next(train_iter)
-            batch = batch.to(device).float()
-            labels = labels.to(device)
-        except StopIteration:  # Start new epoch if run out of data
-            train_iter = iter(trainloader)
-            batch, labels = next(train_iter)
-            batch = batch.to(device).float()
-            labels = labels.to(device)
-        loss= train_step_fn(state, batch, labels)
-
-        #writer.add_scalar("training_loss", loss.item(), step)
-
-        # Save a temporary checkpoint to resume training if training is stopped
-        if step != 0 and step % snapshot_freq_for_preemption == 0:
-            print("Saving temporary checkpoint")
-            save_checkpoint(checkpoint_meta_dir, state)
-
-        # Report the loss on an evaluation dataset periodically
-        if step % eval_freq == 0:
-            print("Starting evaluation")
-            # Use 25 batches for test-set evaluation, arbitrary choice
-            N_evals = 25
-            for i in range(N_evals):
-                try:
-                    eval_batch, labels = next(eval_iter)
-                    eval_batch = batch.to(device).float()
-                    labels = labels.to(device)
-                except StopIteration:  # Start new epoch
-                    eval_iter = iter(testloader)
-                    eval_batch, labels = next(eval_iter)
-                    eval_batch = batch.to(device).float()
-                    labels = labels.to(device)
-                eval_loss = eval_step_fn(state, eval_batch,labels)
-                eval_loss = eval_loss.detach()
-            print("step: %d, eval_loss: %.5e" % (step, eval_loss.item()))
-
-        # Save a checkpoint periodically
-        if step != 0 and step % snapshot_freq == 0 or step == num_train_steps:
-            print("Saving a checkpoint")
-            # Save the checkpoint.
-            save_step = step // snapshot_freq
-            save_checkpoint(os.path.join(
-                checkpoint_dir, 'checkpoint_{}.pth'.format(save_step)), state)
-        
-        if step != 0 and step%sample_freq == 0 or step == num_train_steps:
-            sample()
+def main():
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    checkpoint_dir = os.path.join(os.getcwd(), "checkpoint","base")
+    checkpoint_meta_dir = os.path.join(
+            os.getcwd(), "checkpoint-meta","base", "checkpoint.pth")
+    sample_dir = os.path.join(os.getcwd(), "samples_dir", "base")
 
 
 
 
-    
-sample()
+    """Instantiate dataloaders of training and test datasets"""
+    transform = [transforms.Resize(IMG_SIZE),
+                     transforms.CenterCrop(IMG_SIZE), transforms.ToTensor()]
+    transform = transforms.Compose(transform)
+    training_data = datasets.MNIST(
+                root=".", train=True, download=True, transform=transform)
+    test_data = datasets.MNIST(
+                root=".", train=False, download=True, transform=transform)
+
+
+    trainloader = DataLoader(training_data, batch_size=BATCH_SIZE, shuffle=True, drop_last=True)
+    testloader = DataLoader(test_data, batch_size=BATCH_SIZE, shuffle=True, drop_last=True)
+
+    model = resnet50()
+    model.conv1 = torch.nn.Conv2d(1,64, kernel_size=(7,7), stride = (2,2), padding = (3,3), bias = False)
+
+    model = nn.Sequential(model, nn.Linear(1000,10), nn.LogSoftmax(1))
+
+
+    model.to(device)
+
+    optimizer = Adam(model.parameters(), lr=0.001)
+
+
+
+    """Initialise model state"""
+
+    state = dict(optimizer=optimizer, model=model, step=0)
+    state = restore_checkpoint(checkpoint_meta_dir, state, device)
+    optimize_fn = optimization_manager()
+    train_step_fn = get_step_fn(train=True,optimize_fn=optimize_fn)
+    eval_step_fn = get_step_fn(train=False,optimize_fn=optimize_fn)
+
+
+    initial_step = int(state['step'])
+    train_iter = iter(trainloader)
+    eval_iter = iter(testloader)
+
+    train = True
+
+    if (train == True):
+
+        for step in range(initial_step, num_train_steps + 1):
+            # Train step
+            try:
+                batch, labels = next(train_iter)
+                batch = batch.to(device).float()
+                labels = labels.to(device)
+            except StopIteration:  # Start new epoch if run out of data
+                train_iter = iter(trainloader)
+                batch, labels = next(train_iter)
+                batch = batch.to(device).float()
+                labels = labels.to(device)
+            loss= train_step_fn(state, batch, labels)
+
+            #writer.add_scalar("training_loss", loss.item(), step)
+
+            # Save a temporary checkpoint to resume training if training is stopped
+            if step != 0 and step % snapshot_freq_for_preemption == 0:
+                print("Saving temporary checkpoint")
+                save_checkpoint(checkpoint_meta_dir, state)
+
+            # Report the loss on an evaluation dataset periodically
+            if step % eval_freq == 0:
+                print("Starting evaluation")
+                # Use 25 batches for test-set evaluation, arbitrary choice
+                N_evals = 25
+                for i in range(N_evals):
+                    try:
+                        eval_batch, labels = next(eval_iter)
+                        eval_batch = batch.to(device).float()
+                        labels = labels.to(device)
+                    except StopIteration:  # Start new epoch
+                        eval_iter = iter(testloader)
+                        eval_batch, labels = next(eval_iter)
+                        eval_batch = batch.to(device).float()
+                        labels = labels.to(device)
+                    eval_loss = eval_step_fn(state, eval_batch,labels)
+                    eval_loss = eval_loss.detach()
+                print("step: %d, eval_loss: %.5e" % (step, eval_loss.item()))
+
+            # Save a checkpoint periodically
+            if step != 0 and step % snapshot_freq == 0 or step == num_train_steps:
+                print("Saving a checkpoint")
+                # Save the checkpoint.
+                save_step = step // snapshot_freq
+                save_checkpoint(os.path.join(
+                    checkpoint_dir, 'checkpoint_{}.pth'.format(save_step)), state)
+
+            if step != 0 and step%sample_freq == 0 or step == num_train_steps:
+                sample(model, test_data, device)
+
+    sample(model, test_data, device)
+
+
+if __name__ == '__main__':
+    main()
